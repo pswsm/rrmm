075c96bbce4bedabdf5886d9c8c45be8e4fdb511
Pau Figueras <pau@pswms.cat>
2024-01-14 20:00:23 +0100

    Refine scraping

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,68 @@
 #![allow(dead_code)]
+use core::panic;
+use std::collections::HashMap;
+
 use reqwest::Url;
 use scraper::{Html, Selector};
 
-const SELECTOR: &str = "div.workshopItem > a.ugc";
+const WKSHOP_ITEM_SELECTOR: &str = "div.workshopItem > a.ugc";
+const AUTHOR_NAME_SELECTOR: &str =
+    "div.workshopItem > div.workshopItemAuthorName > a.workshop_author_link";
+
+struct Mod {
+    author: String,
+    id: usize,
+}
+
+fn parse_steam_workshop_url(mod_name: Option<&str>) -> Url {
+    match mod_name {
+        Some(is_mod_name) => Url::parse_with_params(
+            "https://steamcommunity.com/workshop/browse/?appid=294100",
+            &[("searchtext", is_mod_name)],
+        )
+        .unwrap(),
+        None => Url::parse("https://steamcommunity.com/workshop/browse/?appid=294100").unwrap(),
+    }
+}
 
 /// Scrapes the Steam Rimworld workshop for the mod you've searched, or alternativeli shows the recommended ones if no text is provided
-async fn search_workshop(mod_name: Option<&str>) -> Vec<String> {
-    let selector: Selector = match Selector::parse(SELECTOR) {
+async fn search_workshop(mod_name: Option<&str>) -> HashMap<&str, &str> {
+    let workshop_item_selector: Selector = match Selector::parse(WKSHOP_ITEM_SELECTOR) {
         Ok(selector) => selector,
         Err(error) => panic!("{}", error),
     };
-    let workshop_url: Result<Url, _> = match mod_name {
-        Some(is_mod_name) => Url::parse_with_params(
-            "https://steamcommunity.com/workshop/browse/?appid=294100",
-            &[("searchText", is_mod_name)],
-        ),
-        None => Url::parse("https://steamcommunity.com/workshop/browse/?appid=294100"),
+    let author_name_selector: Selector = match Selector::parse(AUTHOR_NAME_SELECTOR) {
+        Ok(selector) => selector,
+        Err(err) => panic!("{}", err),
     };
-    dbg!(&workshop_url);
-    let response = reqwest::get(workshop_url.unwrap()).await;
+
+    let workshop_url: Url = parse_steam_workshop_url(mod_name);
+    let response = reqwest::get(workshop_url).await;
     let response_text = response.unwrap().text().await;
     let html = Html::parse_document(response_text.unwrap().as_str());
-    let scraped = html.select(&selector);
-
-    scraped
+    let scraped_workshop_items = html
+        .select(&workshop_item_selector)
         .into_iter()
-        .map(|v| v.value().attr("data-publishedfileid").unwrap().to_string())
-        .collect::<Vec<String>>()
+        .map(|wkitem| {
+            wkitem
+                .value()
+                .attr("data-publishedfileid")
+                .unwrap()
+                .to_string()
+        })
+        .collect::<Vec<String>>();
+    let scraped_authors = html
+        .select(&author_name_selector)
+        .into_iter()
+        .map(|wkauthor| wkauthor.value())
+        .collect::<Vec<String>>();
+
+    HashMap::from([("", "")])
+
+    // scraped
+    //     .into_iter()
+    //     .map(|v| v.value().attr("data-publishedfileid").unwrap().to_string())
+    //     .collect::<Vec<String>>()
     // return workshop_url.unwrap().as_str().to_owned();
     // return response.unwrap().status().is_success();
 }
